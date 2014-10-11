1694d146c0bd0074cb6d412fffc982e579670554
Hackbright Student <info@hackbrightacademy.com>
2014-10-10 17:41:41 -0700

    updated twitterbot file

--- twitterbot.py
+++ twitterbot.py
@@ -1,5 +1,13 @@
 from markov import *
+import twitter
+from secrets import *
 
+api = twitter.Api()
+
+api = twitter.Api(consumerkey,
+consumersecret, accesstoken, accesstokensecret)
+
+print api.VerifyCredentials()
 
 chain_dict = make_chains()
 print make_text(chain_dict)
